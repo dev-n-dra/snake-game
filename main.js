6fc7020c4e06ac07b37f6dc8610033c43ae8ab25
Devendra Patidar <19patidardevendra@gmail.com>
2021-07-20 19:39:20 +0530

    touch compatibility added

--- main.js
+++ main.js
@@ -276,3 +276,54 @@ function pauseGame() {
 document.addEventListener('keyup', control)
 startButton.addEventListener('click', startGame)
 pauseButton.addEventListener('click', pauseGame)
+
+// touch compatability
+
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+    return evt.touches || // browser API
+        evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) { /*most significant*/
+        if (xDiff > 0) {
+            direction = -1
+        } else {
+            /* right swipe */
+            direction = 1
+        }
+    } else {
+        if (yDiff > 0) {
+            /* up swipe */
+            direction = -width
+        } else {
+            /* down swipe */
+            direction = width
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+};
